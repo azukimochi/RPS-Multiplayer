173e0e67764051559fddd364fa97570ded7d732d
Durian <karen.kua7@gmail.com>
2018-07-05 20:32:22 -0400

    Updated logic for designating player 1 and player 2

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,3 +1,4 @@
+//*** defining global scope variables ***
 var config = {
     apiKey: "AIzaSyCW4ZQjx5hD-Yfa3vxwJJd8cQf6SEO-FQo",
     authDomain: "multi-player-rps-20094.firebaseapp.com",
@@ -9,10 +10,10 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
-// var chatBranch = firebase.database("chat");
-// var playersBranch = firebase.database().ref("players");
-// var playerOneBranch = firebase.database().ref("players/1");
-// var playerTwoBranch = firebase.database().ref("players/2");
+// var chatBranch = firebase.database("chat"); <-delete if not needed
+// var playersBranch = firebase.database().ref("players"); <-delete if not needed
+// var playerOneBranch = firebase.database().ref("players/1"); <-delete if not needed
+// var playerTwoBranch = firebase.database().ref("players/2"); <-delete if not needed
 
 var playerOne = "";
 var playerTwo;
@@ -20,8 +21,37 @@ var winsOne = 0;
 var winsTwo = 0;
 var lossesOne = 0;
 var lossesTwo = 0;
+var turn = 1;
 
+//*** defining functions below ***
 
+//this is the function for submitting the names of player 1 and player 2 
+function designatePlayers() {
+    event.preventDefault;
+    console.log($("Input is " + "#player-input").val());
+    console.log("Before designation: " + playerOne);
+    if (playerOne === "") {
+        playerOne = $("#player-input").val();
+        console.log("After submit, Player One is " + playerOne)
+         playerOne = $("#player-input").val();
+         database.ref("players/1").set({
+            player: playerOne,
+            wins: winsOne,
+            losses: lossesOne,
+        })
+     } else {
+         playerTwo = $("#player-input").val();
+         database.ref("players/2").set({
+            player: playerTwo,
+            wins: winsTwo,
+            losses: lossesTwo,
+         });
+         database.ref("turn").set({
+            turn: turn
+         });
+     } console.log("Player Two is " + playerTwo);
+        console.log("Turn count is " + turn);
+}
 
 //start of function initiation
 $(document).ready(function() {
@@ -33,25 +63,10 @@ $(document).ready(function() {
    }); 
 
    $("#add-player").click(function() {
-       event.preventDefault;
-       console.log($("#player-input").val());
-       console.log(playerOne);
-       if (playerOne === "") {
-            playerOne = $("#player-input").val();
-            database.ref("players/1").set({
-               player: playerOne,
-               wins: winsOne,
-               losses: lossesOne,
-           })
-        } else {
-            playerTwo = $("#player-input").val();
-            database.ref("players/2").set({
-               player: playerTwo,
-               wins: winsTwo,
-               losses: lossesTwo,
-            });
-        } console.log(playerTwo);
+       designatePlayers();
     });
+
+
     // database.ref("players/1").on("value", function(snapshot) {
     //     console.log(snapshot.val());
     //     // highPrice = snapshot.val().highPrice; 
