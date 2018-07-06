96e3e622f47b42e5d336da5e532cabf25f5d322d
Durian <karen.kua7@gmail.com>
2018-07-05 22:39:24 -0400

    added logic for switching player turns and for choosing r/p/s

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -21,9 +21,73 @@ var winsOne = 0;
 var winsTwo = 0;
 var lossesOne = 0;
 var lossesTwo = 0;
-var turn = 1;
+var turnCount = 1;
+var turnOf;
+var playerOneDecision;
+var playerTwoDecision;
 
 //*** defining functions below ***
+
+
+function makePokemonInvisible() {
+    $(".all-pokemon").css("visibility", "hidden");
+    $(".both-scoreboards").css("visibility", "hidden");
+}
+
+function makePokemonVisible() {
+    $(".all-pokemon").css("visibility", "visible");
+    $(".both-scoreboards").css("visibility", "visible");
+}
+
+function turnIsPlayerOne() {
+    $("#player1-text-bar").text("Player 1: " + playerOne + "'s turn");
+    $(".player-one-box").css("border-width", "5px");
+    $(".player-one-box").css("border-color", "red");
+    $(".player-one-box").css("border-style", "groove");
+
+    $("#player2-text-bar").text("Player 2: " + playerTwo);
+    $(".player-two-box").css("border-width", "");
+    $(".player-two-box").css("border-color", "");
+    $(".player-two-box").css("border-style", "");
+
+    turnOf = "turnOfPlayerOne";
+    console.log("Whose turn is it: " +turnOf);
+}
+
+function turnIsPlayerTwo() {
+    $("#player2-text-bar").text("Player 2: " + playerTwo + "'s turn");
+    $(".player-two-box").css("border-width", "5px");
+    $(".player-two-box").css("border-color", "red");
+    $(".player-two-box").css("border-style", "groove");
+
+    $("#player1-text-bar").text("Player 1: " + playerOne);
+    $(".player-one-box").css("border-width", "");
+    $(".player-one-box").css("border-color", "");
+    $(".player-one-box").css("border-style", "");
+
+    turnOf = "turnOfPlayerTwo";
+    console.log("Whose turn is it: " +turnOf);
+}
+
+function addAttrToPokemon() {
+    $("#player-one-rock").attr("data-team", "playerOne");
+    $("#player-one-paper").attr("data-team", "playerOne");
+    $("#player-one-scissor").attr("data-team", "playerOne");
+
+    $("#player-one-rock").attr("data-type", "rock");
+    $("#player-one-paper").attr("data-type", "paper");
+    $("#player-one-scissor").attr("data-type", "scissors");
+
+    $("#player-two-rock").attr("data-team", "playerTwo");
+    $("#player-two-paper").attr("data-team", "playerTwo");
+    $("#player-two-scissor").attr("data-team", "playerTwo");
+
+    $("#player-two-rock").attr("data-type", "rock");
+    $("#player-two-paper").attr("data-type", "paper");
+    $("#player-two-scissor").attr("data-type", "scissors");
+
+}
+
 
 //this is the function for submitting the names of player 1 and player 2 
 function designatePlayers() {
@@ -33,6 +97,7 @@ function designatePlayers() {
     if (playerOne === "") {
         playerOne = $("#player-input").val();
         console.log("After submit, Player One is " + playerOne)
+        $("#player1-text-bar").text("Player 1: " + playerOne + " has entered the stadium!");
          playerOne = $("#player-input").val();
          database.ref("players/1").set({
             player: playerOne,
@@ -41,26 +106,53 @@ function designatePlayers() {
         })
      } else {
          playerTwo = $("#player-input").val();
+         $("#player2-text-bar").text("Player 2: " + playerTwo + " has entered the stadium!");
          database.ref("players/2").set({
             player: playerTwo,
             wins: winsTwo,
             losses: lossesTwo,
          });
-         database.ref("turn").set({
-            turn: turn
+         database.ref("turn-counter").set({
+            turn: turnCount
          });
+         makePokemonVisible();
+         turnIsPlayerOne();
      } console.log("Player Two is " + playerTwo);
-        console.log("Turn count is " + turn);
+        console.log("Turn count is " + turnCount);
 }
 
 //start of function initiation
 $(document).ready(function() {
-   $(".all-pokemon").click(function() {
-       var choiceStatement = "<p>Player Two chose"
-       console.log(this);
-       $(this).prepend(choiceStatement);
-       $("#player-two-choice").append(this);
-   }); 
+
+    makePokemonInvisible();
+    addAttrToPokemon();
+
+    $(".all-pokemon").click(function () {
+        if (turnOf === "turnOfPlayerOne") {
+            if ($(this).attr("data-team") === "playerOne") {
+                var choiceStatement = "<p>Player 1 chose"
+                console.log(this);
+                console.log("The team this is on: " + $(this).attr("data-team"));
+                console.log("The type: " + $(this).attr("data-type"));
+                $(this).prepend(choiceStatement);
+                $("#player-one-choice").append(this);
+                playerOneDecision = $(this).attr("data-type");
+                turnIsPlayerTwo();
+            } 
+        } else if (turnOf === "turnOfPlayerTwo") {
+            if ($(this).attr("data-team") === "playerTwo") {
+                var choiceStatement = "<p>Player 2 chose"
+                console.log(this);
+                console.log("The team this is on: " + $(this).attr("data-team"));
+                console.log("The type: " + $(this).attr("data-type"));
+                $(this).prepend(choiceStatement);
+                $("#player-two-choice").append(this);
+                playerTwoDecision = $(this).attr("data-type");
+            }
+        }
+        console.log("Player 1's Decision: " + playerOneDecision);
+        console.log("Player 2's Decision: " + playerTwoDecision);
+    }); 
 
    $("#add-player").click(function() {
        designatePlayers();
